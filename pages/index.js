06256ea307b2efc6e6f6ba2245c927fd5f1b703d
Dave Roverts <me@daveroverts.nl>
2021-07-18 11:56:47 +0200

    Add image from old site

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import { NextSeo } from "next-seo";
 import Layout from "../components/Layout";
+import Image from 'next/image'
+import security_advice from 'public/static/images/index/security_advice.png'
 
 export default function Index({ posts }) {
   const title = 'Home'
   return (
     <>
       <NextSeo title={title} />
-      <Layout title="Hello there! 👋" subtitle="I'm Dave Roverts, 24 years old and from the Netherlands 🇳🇱. Web developer 🖥 by day, and flight simmer ✈️ in the evening.">
-        <div className="py-5">
+      <Layout title="Handjes! Handjes! Bloemetjesgordijn!">
+        <div className="py-5 text-center">
+          <Image src={security_advice} alt="Security advice" />
           {/* <h3 className="text-2xl font-bold">Latest posts</h3>
           <div className="grid space-x-2 lg:grid-cols-2">
 
